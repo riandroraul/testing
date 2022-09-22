08b6b8e59efae7ee89e700cdb1e3a9534a34757b
riandroraul <riandroraul@gmail.com>
2022-09-22 16:59:15 +0700

    first commit

--- controller/bigStorage.js
+++ controller/bigStorage.js
@@ -2,8 +2,14 @@ const Item = require("../models/items");
 const SmallStorage = require("../models/smallStorage");
 const BigStorage = require("../models/bigStorage");
 
-const saveItemToBigStorage = async () => {
+const saveItemToBigStorage = async (req, res) => {
   try {
+    const item = Item.findOne({ _id: req.body.idItem });
+    const storage = SmallStorage.findOne({ _id: req.body.idStg });
+    if (!item || !storage) {
+      throw new Error("id item or id storage not found", res.status(400));
+    }
+    res.status(200).json({ message: "success" });
   } catch (error) {
     console.log(error.message);
     res.json({
@@ -12,7 +18,7 @@ const saveItemToBigStorage = async () => {
     });
   }
 };
-const searchItemInBigStorage = async () => {
+const searchItemInBigStorage = async (req, res) => {
   try {
   } catch (error) {
     console.log(error.message);
