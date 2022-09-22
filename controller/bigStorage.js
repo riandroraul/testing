b74f73904decdcb75ba0f10e3ae06b9f1c5c50b2
riandroraul <riandroraul@gmail.com>
2022-09-22 16:52:36 +0700

    first commit

--- controller/bigStorage.js
+++ controller/bigStorage.js
@@ -1,4 +1,26 @@
-const saveItemToBigStorage = async () => {};
-const searchItemInBigStorage = async () => {};
+const Item = require("../models/items");
+const SmallStorage = require("../models/smallStorage");
+const BigStorage = require("../models/bigStorage");
+
+const saveItemToBigStorage = async () => {
+  try {
+  } catch (error) {
+    console.log(error.message);
+    res.json({
+      message: error.message,
+      status: 400,
+    });
+  }
+};
+const searchItemInBigStorage = async () => {
+  try {
+  } catch (error) {
+    console.log(error.message);
+    res.json({
+      message: error.message,
+      status: 400,
+    });
+  }
+};
 
 module.exports = { saveItemToBigStorage, searchItemInBigStorage };
